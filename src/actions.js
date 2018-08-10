3df979bff24de8c757a35607ecaa3b39052eaa7c
github username <github email address>
2018-08-10 18:04:05 -0400

    thunk working

---
+++ src/actions.js
@@ -0,0 +1,34 @@
+
+import { UPDATE_ARTWORK_ACTION, ARTWORK_FETCH_SUCCESSFUL } from './types';
+
+export function incrementAction() {
+  return { type: 'INCREMENT' }
+}
+
+export function incrementFriendsAction() {
+  return { type: 'INCREMENT_FRIENDS' }
+}
+
+export function addFriendAction(friend) {
+  return { type: "ADD_FRIEND", payload: friend }
+}
+
+export function setCountAction(count) {
+  return { type: 'SET_COUNT', payload: count }
+}
+
+/// MY FUNCTIONS
+
+// export function updateArtworkAction(artworkArray) {
+//   return { type: UPDATE_ARTWORK_ACTION, payload: artworkArray }
+// }
+
+export function artworkFetch(url) {
+    return (dispatch) => {
+      return fetch(url)
+            .then((response) => response.json())
+            .then((artworkArray) => {
+              dispatch({type: ARTWORK_FETCH_SUCCESSFUL, payload: artworkArray})
+            })
+    }
+}
---
+++ src/reducer.js
@@ -0,0 +1,26 @@
+import { ADD_FRIEND } from './types';
+import { UPDATE_ARTWORK_ACTION, ARTWORK_FETCH_SUCCESSFUL } from "./types";
+
+//default state here
+const initialState = {
+  artworkArray: [],
+}
+
+function reducer (state = initialState, action) {
+  switch(action.type){
+    case "INCREMENT":
+      return {...state, count: state.count + 1, somethingElse: "new"}
+    case "DECREMEMNT":
+      return {...state, count: state.count - 1, somethingElse: "newer"}
+    case ADD_FRIEND:
+      return {...state, friendsArray: [...state.friendsArray, action.payload]}
+    // case UPDATE_ARTWORK_ACTION:
+    //   return {...state, artworkArray: [...state.artworkArray, action.payload]}
+    case ARTWORK_FETCH_SUCCESSFUL:
+      return {...state, artworkArray: [...state.artworkArray, action.payload]}
+    default:
+      return state;
+  }
+}
+
+export default reducer
