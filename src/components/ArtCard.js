74d84f2ef06d1ca2daaee055be720aca5819d442
github username <github email address>
2018-08-10 10:24:21 -0400

    cleaned up bad code

--- src/components/ArtCard.js
+++ src/components/ArtCard.js
@@ -5,8 +5,7 @@ class ArtCard extends Component {
     console.log(this.props)
     return(
       <div className="div--art-card">
-        <h1>{this.props.title}</h1>
-        <h1>{this.props.artist}</h1>
+
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  constructor(props){
-    super(props);
 
-    this.state = {
-      currentQuery: "",
-    }
-  }
-
-  handleChange = (event) => {
-    this.setState({currentQuery: event.target.value})
-  }
 
   render(){
     // console.log("Current state in SearchBar", this.state.currentQuery)
     return(
-      <div className="search-bar div--search-bar">
-        <form onSubmit={this.props.onSearchSubmit}>
-          <input type="text"
-            value={this.state.currentQuery}
-            onChange={this.handleChange}
-            className="input input--search-bar"
-            placeholder="Search here for art"
-            ></input>
-          <button className="button button--search-bar">Press me to see some art</button>
-        </form>
+      <div>
       </div>
     );
   }
